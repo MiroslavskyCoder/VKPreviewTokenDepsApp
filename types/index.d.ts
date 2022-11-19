7e74f45c8037b3984967784869ad5cfc362d4013
MiroslavskyCoder <alinowski2016@yandex.ru>
2022-11-19 19:22:57 +0300

    update: ts types file

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,12 +2,23 @@ namespace VKPreviewToken {
     interface Session { 
         token: string;
     }
+    interface Versions { 
+        node(): string;
+        chrome(): string;
+        electron(): string;
+    }
 }
 
 interface VKPreviewToken {
     listSavesSessions(options?: any): VKPreviewToken.Session[];
     createSession(options: any): VKPreviewToken.Session;
     isActiveSession(): boolean;
+
+    versions: VKPreviewToken.Versions;
+
+    exit(code?: number): void;
+
+    isDev(): boolean;
 }
 
 interface Window {
